31361363dfd9d9886d3f020a5d6e8b6bc695ad45
FreVanHorebeek <144011725+FreVanHorebeek@users.noreply.github.com>
2023-09-27 20:08:14 +0200

    nummer 2

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
 
     [SerializeField] private Text cherriesText;
-
     [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void Start()
+    {
+        // Lees het aantal verzamelde kersen uit PlayerPrefs voor het huidige level
+        cherries = PlayerPrefs.GetInt("Cherries", 0);
+        UpdateCherriesText();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
@@ -17,7 +25,17 @@ public class ItemCollector : MonoBehaviour
             Destroy(collision.gameObject);
             cherries++;
             Debug.Log("Cherries: " + cherries);
-            cherriesText.text = "Cherries: " + cherries;
+
+            // Sla het aantal verzamelde kersen op in PlayerPrefs voor het huidige level
+            PlayerPrefs.SetInt("Cherries", cherries);
+            PlayerPrefs.Save();
+
+            UpdateCherriesText();
         }
+    }
+
+    private void UpdateCherriesText()
+    {
+        cherriesText.text = "Cherries: " + cherries;
     }
 }
